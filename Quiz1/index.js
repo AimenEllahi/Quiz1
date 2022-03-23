9221359aa96c1804b809c61362b7a0baeb7123f3
AimenEllahi <98141911+AimenEllahi@users.noreply.github.com>
2022-03-23 15:04:48 +0500

    Add files via upload

---
+++ Quiz1/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  
+    $("#ripples-style").click(function(){
+        
+    })
+    $('#ripples-style').ripples(
+        {
+            resolution:512,
+            dropRadius:20
+        }
+    )
+
+})

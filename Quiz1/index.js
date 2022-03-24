0c44780efbfca16f2b6e475d3aa1a9a193160b46
Hanan Munir <66138578+hananmunir@users.noreply.github.com>
2022-03-24 20:37:40 +0500

    I guess its okay now

--- Quiz1/index.js
+++ Quiz1/index.js
@@ -1,28 +1,9 @@
 $(document).ready(function () {
   //creates progress bars
   $(".circlechart").circlechart();
-
   // creates ripples
   $("#ripples-style").ripples({
     resolution: 512,
     dropRadius: 20,
   });
 });
-
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function (event) {
-  if (!event.target.matches(".dropbtn")) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains("show")) {
-        openDropdown.classList.remove("show");
-      }
-    }
-  }
-};
